ec686dbbd5e067f664d2568a126a963ad15d1d93
Mark Lintern <markllintern@gmail.com>
2011-11-05 09:38:42 -0400

    update IPtest script to have multiple users on it

--- IPTest.sh
+++ IPTest.sh
@@ -2,9 +2,10 @@
 
 ipa=$(cat ~/outsideIP.txt)
 ipb=$(curl icanhazip.com)
+TO='mlintern@compendium.com aparker@compendium.com'
 
 if [ $ipa != $ipb ]
   then
-	  echo "The Outside IP address has changed from $ipa to $ipb" | mail -s "IP Address Change" mlintern@compendium.com
+	  echo "The Outside IP address has changed from $ipa to $ipb" | mail -s "IP Address Change $TO"
 	  echo $ipb > ~/outsideIP.txt
 fi
